c317f7f0c70ad6b38734933200e6811a089d7ed7
sublime247 <kushimobashir2000@gmail.com>
2025-04-29 12:11:36 +0100

    collector+api

--- packages/build-ui/src/pages/agits/_id/management/collectors/controllers.rs
+++ packages/build-ui/src/pages/agits/_id/management/collectors/controllers.rs
@@ -1,45 +1,58 @@
 #![allow(unused)]
 use std::io::Read;
 
-use bdk::prelude::{dioxus_popup::PopupService, *};
-
-use crate::pages::agits::_id::management::{Activity, Assets};
-
-use super::models::{Collector};
 use super::models::*;
+use crate::pages::agits::_id::management::{Activity, Assets};
+use bdk::prelude::{dioxus_popup::PopupService, *};
+use wasm_bindgen_futures::spawn_local;
+use common::tables::{
+    collectors::Collector as CollectorModel,
+    prelude::{CollectorByIdAction, CollectorCreateRequest, CollectorDeleteRequest, CollectorQuery},
+};
+
+
 
 
 #[derive(Debug, Clone, Copy, DioxusController)]
-pub struct Controller{
+pub struct Controller {
     lang: Language,
     collector: Signal<Vec<Collector>>,
     asset: Signal<Vec<Assets>>,
     activity: Signal<Vec<Activity>>,
-    popup: PopupService
-
- 
+    popup: PopupService,
 }
-impl Controller{
-  pub fn new(lang: Language, agit_id: ReadOnlySignal<i64>)-> Result<Self, RenderError>{
-    let mut popup :PopupService = use_context();
-    let collector = use_signal(||{
-        (1..15)
-        .map(|id| Collector{
-            id: id.to_string(),
-            total_volume: 2.370,
-            total_volume_usd: 8147.63,
-            collector_id: "10FEO!20".to_string(),
-            owned: 2,
-            token_ids: vec!["1234567890".to_string(), "1234567890".to_string()],
-            wallet_address: "0x1234567890abcdef".to_string(),
-            last_activity: "2023-10-01".to_string(),
-            verified: true,
-        })
-        .collect::<Vec<_>>()
-    });
-   
- let asset = use_signal(||{
-    (0..8).map(|id| Assets{
+impl Controller {
+    pub fn new(lang: Language, agit_id: ReadOnlySignal<i64>) -> Result<Self, RenderError> {
+        let mut popup: PopupService = use_context();
+
+        let res = use_server_future(move || async move {
+            let endpoint = crate::config::get().api_url;
+            let client = CollectorModel::get_client(endpoint);
+            client
+                .query(CollectorQuery::new(100).with_page(0))
+                .await
+                .unwrap_or_default()
+        })?;
+        tracing::debug!("res: {:?}", res);
+
+        let collector = use_signal(|| {
+            (1..15)
+                .map(|id| Collector {
+                    id: id.to_string(),
+                    total_volume: 2.370,
+                    total_volume_usd: 8147.63,
+                    collector_id: "10FEO!20".to_string(),
+                    owned: 2,
+                    token_ids: vec!["1234567890".to_string(), "1234567890".to_string()],
+                    wallet_address: "0x1234567890abcdef".to_string(),
+                    last_activity: "2023-10-01".to_string(),
+                    verified: true,
+                })
+                .collect::<Vec<_>>()
+        });
+
+        let asset = use_signal(|| {
+            (0..8).map(|id| Assets{
         id : id.to_string(),
         title: "Asset Title".to_string(),
         artist_name: "Artist Name".to_string(),
@@ -63,26 +76,84 @@ impl Controller{
         rarity: "Rare".to_string(),
 
     }).collect::<Vec<_>>()
- });
+        });
 
- let activity = use_signal(||{
-    (0..6).map(|id|Activity{
-        id: id.to_string(),
-        from: "20114FWO".to_string(),
-        to: "20114FWO".to_string(),
-        time: "30 mins ago".to_string(),
-        title: "Art Title".to_string()
-    }).collect::<Vec<_>>()
- });
+        let activity = use_signal(|| {
+            (0..6)
+                .map(|id| Activity {
+                    id: id.to_string(),
+                    from: "20114FWO".to_string(),
+                    to: "20114FWO".to_string(),
+                    time: "30 mins ago".to_string(),
+                    title: "Art Title".to_string(),
+                })
+                .collect::<Vec<_>>()
+        });
 
-    let ctrl = Self{
-        lang,
-        collector,
-        popup,
-        asset,
-        activity
-    };
-    use_context_provider(||ctrl);
-    Ok(ctrl)
+        let ctrl = Self {
+            lang,
+            collector,
+            popup,
+            asset,
+            activity,
+        };
+        use_context_provider(|| ctrl);
+        Ok(ctrl)
+    }
+  pub fn create_collector(&self) {
+    spawn_local(async move{
+        let endpoint = crate::config::get().api_url;
+        let client = CollectorModel::get_client(endpoint);
+        let res = client.act(
+            common::tables::prelude::CollectorAction::Create(
+                CollectorCreateRequest {
+                    title: "".to_string(),
+                    description: "".to_string(),
+                    external_link: None,
+                    banner_url: "".to_string(),
+                    logo_url: "".to_string(),
+                },
+            )
+        ).await;
+        match res {
+            Ok(_) => {
+                tracing::debug!("Collector created successfully");
+            }
+            Err(err) => {
+                tracing::error!("Error creating collector: {:?}", err);
+            }
+            
+        }
+    });
   }
+
+  pub fn remove_collector(&self, collector_id:i64){
+
+   spawn_local(async move{
+    let endpoint = crate::config::get().api_url;
+    let client = CollectorModel::get_client(endpoint);
+    let res = client.act_by_id(
+        collector_id,
+        CollectorByIdAction::Delete(CollectorDeleteRequest {}),
+    ).await;
+    match res {
+        Ok(_) => {
+            tracing::debug!("Collector removed successfully");
+        }
+        Err(err) => {
+            tracing::error!("Error removing collector: {:?}", err);
+        }
+        
+    }
+
+   });
+
+
+
+
+  }
+  
+
+
+
 }
