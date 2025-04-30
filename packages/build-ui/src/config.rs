6bcd5d88cdcccd8807577a22a442f8b8ddcce38e
ryancho-0 <ryan@biyard.co>
2025-04-30 10:52:42 +0900

    feat(user-ui): add firebase config

--- packages/build-ui/src/config.rs
+++ packages/build-ui/src/config.rs
@@ -1,11 +1,12 @@
 use bdk::prelude::dioxus_logger::tracing::Level;
-
+use dioxus_oauth::firebase::FirebaseConfig;
 #[derive(Debug)]
 pub struct Config {
     pub env: &'static str,
     pub commit: &'static str,
     pub log_level: Level,
     pub api_url: &'static str,
+    pub firebase: FirebaseConfig,
 }
 
 impl Default for Config {
@@ -22,6 +23,29 @@ impl Default for Config {
                 _ => Level::INFO,
             },
             api_url: option_env!("API_URL").unwrap_or("https://dagit-api.dev.biyard.co"),
+            firebase: FirebaseConfig {
+                api_key: option_env!("FIREBASE_API_KEY")
+                    .expect("You must set FIREBASE_API_KEY")
+                    .to_string(),
+                auth_domain: option_env!("FIREBASE_AUTH_DOMAIN")
+                    .expect("You must set FIREBASE_AUTH_DOMAIN")
+                    .to_string(),
+                project_id: option_env!("FIREBASE_PROJECT_ID")
+                    .expect("You must set FIREBASE_PROJECT_ID")
+                    .to_string(),
+                storage_bucket: option_env!("FIREBASE_STORAGE_BUCKET")
+                    .expect("You must set FIREBASE_STORAGE_BUCKET")
+                    .to_string(),
+                messaging_sender_id: option_env!("FIREBASE_MESSAGING_SENDER_ID")
+                    .expect("You must set FIREBASE_MESSAGING_SENDER_ID")
+                    .to_string(),
+                app_id: option_env!("FIREBASE_APP_ID")
+                    .expect("You must set FIREBASE_APP_ID")
+                    .to_string(),
+                measurement_id: option_env!("FIREBASE_MEASUREMENT_ID")
+                    .expect("You must set FIREBASE_MEASUREMENT_ID")
+                    .to_string(),
+            },
         }
     }
 }
