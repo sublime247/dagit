1e59865c93d27b982f719c97aaff34855676ee34
sublime247 <kushimobashir2000@gmail.com>
2025-05-03 01:33:11 +0100

    dx fmt

--- packages/build-ui/src/pages/agits/_id/management/components/nft_table.rs
+++ packages/build-ui/src/pages/agits/_id/management/components/nft_table.rs
@@ -23,43 +23,11 @@ pub fn NftTable(assets: Vec<Assets>, lang: Language) -> Element {
                                 class: "overflow-hidden hover:border-gray-600 transition-all duration-200 flex flex-col",
                                 // Image container
                                 div { class: "relative bg-gray-800", style: "{dimensions}",
-<<<<<<< HEAD
-=======
-
->>>>>>> 5998d294a1a35dfaaacd7be9f3ad2923ed2f3fcd
                                     div { class: "text-4xl text-gray-600 absolute inset-0 flex items-center justify-center",
                                         img {
                                             class: "w-full h-full object-cover",
                                             src: asset.art_image.clone(),
                                         }
-<<<<<<< HEAD
-                                    }
-                                }
-                                // NFT metadata section
-                                div { class: "py-4 text-white flex-shrink-0",
-                                    // Title and tag
-                                    div { class: "flex items-center mb-2",
-                                        span { class: "text-lg font-semibold", {asset.title.clone()} }
-                                        span { class: "ml-2 text-sm border border-gray-600 px-2 py-1", "TAG" }
-                                    }
-                                    // Artist name
-                                    div { class: "text-sm text-gray-400 mb-2", "by {asset.artist_name}" }
-                                    // Price, Sales Volume, and Owners
-                                    div { class: "flex justify-between text-gray-400  text-sm",
-                                        div { class: "flex flex-col",
-                                            div { {tr.current_price} }
-                                            span { class: "font-bold text-white", "${asset.current_price}" }
-                                        }
-                                        div { class: "flex flex-col",
-                                            div { {tr.sales_volume} }
-                                            span { class: "font-bold text-white", "${asset.volume}" }
-                                        }
-                                        div { class: "flex flex-col",
-                                            div { {tr.owner} }
-                                            span { class: "font-bold text-white", "{asset.owner}" }
-                                        }
-                                    }
-=======
 
                                     }
                                 }
@@ -89,7 +57,6 @@ pub fn NftTable(assets: Vec<Assets>, lang: Language) -> Element {
                                             span { class: "font-bold text-white", "{asset.owner}" }
                                         }
                                     }
->>>>>>> 5998d294a1a35dfaaacd7be9f3ad2923ed2f3fcd
                                 }
                             }
                         }
