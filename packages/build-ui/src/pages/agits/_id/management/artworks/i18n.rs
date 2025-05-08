ed275af32294ccce7d79ea97ff87d77f66e1b4c7
sublime247 <kushimobashir2000@gmail.com>
2025-05-08 07:28:57 +0100

    new

--- packages/build-ui/src/pages/agits/_id/management/artworks/i18n.rs
+++ packages/build-ui/src/pages/agits/_id/management/artworks/i18n.rs
@@ -1,1 +1,41 @@
+use bdk::prelude::*;
 
+translate!{
+    ArtworkTranslate;
+    artwork:{
+     en:"Artwork",
+     ko:"작품",
+    }
+    title:{
+     en:"Title",
+     ko:"제목",
+    }
+    attributes:{
+     en:"Attributes",
+     ko:"속성",
+    }
+    ways_to_sell:{
+     en:"Ways to Sell",
+     ko:"판매 방법",
+    }
+    owner:{
+     en:"Owner",
+     ko:"소유한",
+    }
+    current_price:{
+     en:"Current Price",
+     ko:"현재 가격",
+    }
+    average_price:{
+     en:"Average Price",
+     ko:"평균 가격",
+    }
+    price_change:{
+     en:"Price Change",
+     ko:"가격 변화",
+    }
+    volume:{
+     en:"Volume",
+     ko:"거래량",
+    }
+}
