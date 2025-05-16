5039e7c544b213a8c06a75c1e5b8213554445ed4
sublime247 <kushimobashir2000@gmail.com>
2025-05-16 07:56:03 +0100

    tables added to the migration micro

--- packages/common/src/tables/mod.rs
+++ packages/common/src/tables/mod.rs
@@ -5,7 +5,6 @@ pub mod artworks;
 pub mod collections;
 pub mod collectors;
 pub mod users;
-pub mod dao;
 pub mod categories;
 pub mod topics;
 
@@ -19,7 +18,6 @@ pub mod prelude {
     pub use super::collections::*;
     pub use super::collectors::*;
     pub use super::users::*;
-    pub use super::dao::*;
     pub use super::categories::*;
     pub use super::topics::*;
 }
