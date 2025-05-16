d43fc32c949e8d96e4d8262a4a0079cb53125f19
sublime247 <kushimobashir2000@gmail.com>
2025-05-16 06:40:53 +0100

    changes the table to categories

--- packages/common/src/tables/categories/categories.rs
+++ packages/common/src/tables/categories/categories.rs
@@ -6,8 +6,8 @@ pub struct Category {
     pub id: i64,
     #[api_model(summary, many_to_one = agits)]
     pub agit_id: i64,
-    #[api_model(summary, action = create, action_by_id = update, nullable)]
+    #[api_model(summary, action = create, action_by_id = update)]
     pub title: String,
-    #[api_model(summary, action = create, action_by_id = update, nullable)]
+    #[api_model(summary, action = create, action_by_id = update)]
     pub description: String,
 }
